d4a94a5a0d98dfe3997d10f542e1513233ae01b2
Adonis Gaitatzis <backupbrain@gmail.com>
2022-09-14 18:05:12 +0200

    adding freshbook oauth api

--- freshbooks-oauth-api/src/server.ts
+++ freshbooks-oauth-api/src/server.ts
@@ -4,17 +4,22 @@ const port = 9991;
 app.use(express.json());
 
 app.get("/api/1.0/oauth", (req: Request, res: Response) => {
-  console.log(req.body);
+  console.log(req.params);
+  console.log(`Method: ${req.method}`);
   res.send("Hello");
 });
 
 app.post("/api/1.0/oauth", (req: Request, res: Response) => {
   console.log(req.body);
+  console.log(req.params);
+  console.log(`Method: ${req.method}`);
   res.send("Hello");
 });
 
 app.put("/api/1.0/oauth", (req: Request, res: Response) => {
   console.log(req.body);
+  console.log(req.params);
+  console.log(`Method: ${req.method}`);
   res.send("Hello");
 });
 
