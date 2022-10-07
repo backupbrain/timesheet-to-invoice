61f0a0845057f58a86d99a538c854dc5d3669ad0
Adonis Gaitatzis <backupbrain@gmail.com>
2022-10-07 02:07:49 +0200

    making notes

--- harvest2freshbooks/src/scrapeHarvest.ts
+++ harvest2freshbooks/src/scrapeHarvest.ts
@@ -3,3 +3,8 @@ import { getDataFromHarvest } from "./jobs/getDataFromHarvest";
 getDataFromHarvest().then(() => {
   console.log("done");
 });
+
+// after scrapting from harvest,
+// we must put int the Address and Client.addressId
+// also the Client.freshbooksId and Client.freshbooksName
+// for each client
